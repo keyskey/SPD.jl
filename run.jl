41cd1cc281aa63414b1fd850b65bfbc95867de86
Keisuke Nagashima <energyrevo21@gmail.com>
2019-02-02 00:09:39 +0900

    Delete conflict message

--- run.jl
+++ run.jl
@@ -1,11 +1,11 @@
-@everywhere include("simulation.jl")
-using .Simulation
 using PyCall
 using Distributed
 @pyimport networkx as nx
+@everywhere include("simulation.jl")
+using .Simulation
  
-const num_episode = 100
-const population = 100
+const num_episode = 1
+const population = 10000
 const topology = nx.random_regular_graph(8, population) 
 
 Distributed.pmap(episode -> Simulation.one_episode(episode, population, topology), 1:num_episode)
--- simulation.jl
+++ simulation.jl
@@ -1,15 +1,10 @@
 include("decision.jl")
 
 module Simulation
-<<<<<<< HEAD
-=======
-    using ..Society
->>>>>>> 87e446c43dab90f3ffbb989b0007a96754b27423
     using ..Decision
     using ..Society
     using CSV
     using DataFrames
-<<<<<<< HEAD
     using Statistics
     using Random
     
@@ -22,24 +17,9 @@ module Simulation
             count_payoff(society, dg, dr)
             pairwise_fermi(society, 1/beta)
             fc = count_fc(society)
-            #println("Episode: $(episode), Beta: $(beta), Dg: $(dg), Dr: $(dr), Time: $(step), Fc:$(fc)")
+            println("Episode: $(episode), Beta: $(beta), Dg: $(dg), Dr: $(dr), Time: $(step), Fc:$(fc)")
             push!(fc_hist, fc)
 
-=======
-    using Random
-    
-    function time_loop(society, init_c, dg, dr)
-        initialize_strategy(society, init_c)
-        init_fc = count_fc(society)
-        fc_hist = [init_fc]
-        println("Dg: $(dg), Dr: $(dr), Time: 0, Fc:$(init_fc)")
-        for step = 1:1000
-            count_payoff(society, dg, dr)
-            pairwise_fermi(society)
-            fc = count_fc(society)
-            println("Dg: $(dg), Dr: $(dr), Time: $(step), Fc:$(fc)")
-            push!(fc_hist, fc) 
->>>>>>> 87e446c43dab90f3ffbb989b0007a96754b27423
             if fc == 0 || fc == 1 || step == 1000
                 global solution = fc
                 break
@@ -52,7 +32,6 @@ module Simulation
         return solution
     end
 
-<<<<<<< HEAD
     function one_episode(episode::Int, population::Int, topology)
         Random.seed!()
         society::SocietyType = SocietyType(population, topology)
@@ -64,21 +43,7 @@ module Simulation
                     fc = time_loop(society, init_c, episode, beta, dg, dr)
                     DataFrame(Beta = [beta], Dg = [dg], Dr = [dr], Fc = [fc]) |> CSV.write("episode_$(episode).csv", append=true)
                 end
-=======
-    function one_episode(episode, population, topology)
-        society = SocietyType(population, topology)
-        DataFrame(Dg = [], Dr = [], Fc = []) |> CSV.write("episode_$(episode).csv")
-        init_c = choose_initial_cooperators(population)
-        for dg = 0:0.1:1
-            for dr = 0:0.1:1
-                fc = time_loop(society, init_c, dg, dr)
-                DataFrame(Dg = [dg], Dr = [dr], Fc = [fc]) |> CSV.write("episode_$(episode).csv", append=true)
->>>>>>> 87e446c43dab90f3ffbb989b0007a96754b27423
             end
         end
     end
 end
-<<<<<<< HEAD
-
-=======
->>>>>>> 87e446c43dab90f3ffbb989b0007a96754b27423
