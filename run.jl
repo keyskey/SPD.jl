9e6690c8b74db5f1b59650b454392fdb71ff78cf
Keisuke Nagashima <energyrevo21@gmail.com>
2019-08-28 15:32:23 +0900

    refactor and add comment for possible network

--- run.jl
+++ run.jl
@@ -1,15 +1,12 @@
-# Before running this code in parallel, please remove the comment out in the line 5,17 and comment out the line 6 and 16. Then run;
-# $ julia -p (number of process) run.jl
+# For parallel execution, run with $ julia -p (number of process) run.jl
 
 using Distributed
-# @everywhere include("simulation.jl")
-include("simulation.jl")
+@everywhere include("simulation.jl")
 using .Simulation
- 
-const num_episode = 1
-const population = 100
-const average_degree = 8
-const topology_name = "Lattice"
 
-map(episode -> Simulation.one_episode(episode, population, average_degree, topology_name), 1:num_episode)
-#Distributed.pmap(episode -> Simulation.one_episode(episode, population, topology), 1:num_episode)
+const num_episode = 100
+const population = 10000
+const average_degree = 8
+const topology_type = "Ring"  # ER | ScaleFree | Lattice | Ring
+
+Distributed.pmap(episode -> Simulation.one_episode(episode, population, average_degree, topology_type), 1:num_episode)
