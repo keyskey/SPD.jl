660b8a9f758a11e03527085c7f39a574731c3649
Keisuke Nagashima <energyrevo21@gmail.com>
2020-01-07 17:21:02 +0900

    fix simulation module

--- simulation.jl
+++ simulation.jl
@@ -1,51 +1,56 @@
-include("./society.jl")
+include("./game.jl")
 
 module Simulation
-using ..Society
-using CSV
-using DataFrames
-using Statistics
-using Random
+using CSV, DataFrames, Statistics, Random, LightGraphs
+using ..Game
 export one_episode
 
-function time_loop(society::SocietyType, initial_cooperators::Vector{Int}, episode::Int, dg, dr, kappa)
-    tmax = 1000
-    initialize_strategy!(society, initial_cooperators)
-    init_fc = count_fc(society)
-    fc_hist = [init_fc]
+function game_loop(model::Game.Model, payoff_matrix::Game.PayoffMatrix, episode::Int)
+    Game.initialize_strategy!(model)
+    initial_fc = count_fc(model)
+    fc_hist = [initial_fc]
+    dg, dr = payoff_matrix.dg, payoff_matrix.dr
+    println("Episode: $(episode), Dg: $(dg), Dr: $(dr), Timestep: 0, Fc: $(initial_fc)")
 
-    println("Episode: $(episode), Dg: $(dg), Dr: $(dr), Time: 0, Fc:$(init_fc)")
-    for step in 1:tmax
-        fc = count_payoff!(society, dg, dr) |> (s -> pairwise_fermi!(s, kappa)) |> count_fc
-        println("Episode: $(episode), Dg: $(dg), Dr: $(dr), Time: $(step), Fc:$(fc)")
+    step = 0
+    while true
+        step += 1
+        Game.count_payoff!(model, payoff_matrix)
+        Game.pairwise_fermi!(model)
+        fc = Game.count_fc(model)
+        println("Episode: $(episode), Dg: $(dg), Dr: $(dr), Timestep: $(step), Fc: $(fc)")
         push!(fc_hist, fc)
-        if fc == 0 || fc == 1
-            global solution = fc
-            break
-        elseif (step >= 100 && (Statistics.mean(fc_hist[end-100:end-1])-fc)/fc <= 0.001) || step == tmax
-            global solution =  Statistics.mean(fc_hist[end-99:end])
-            break
-        end
-    end
-    return solution
-end
 
-function one_episode(episode::Int, population::Int, average_degree::Int, topology_type::AbstractString)
-    Random.seed!()
-    society = SocietyType(population, average_degree, topology_type)
-
-    DataFrame(Dg = [], Dr = [], Fc = []) |> CSV.write("./results/episode_$(episode).csv")
-    initial_cooperators = choose_initial_cooperators(population)
-    beta_range = (0.1, 1, 10)
-    kappa_range = 1 ./ beta_range
-    for kappa in kappa_range
-        for dg in -1:0.1:1
-            for dr in -1:0.1:1
-                fc = time_loop(society, initial_cooperators, episode, dg, dr, kappa)
-                DataFrame(Dg = [dg], Dr = [dr], Fc = [fc]) |> CSV.write("./results/episode_$(episode).csv", append=true)
+        if step >= 100
+            if (Statistics.mean(fc_hist[end-100:end-1]) - fc) / fc <= 0.001
+                global solution = Statistics.mean(fc_hist[end-99:end])
+                break
+            else fc == 0 || 1
+                global solution = fc
+                break
             end
         end
     end
+
+    return solution
+end
+
+function one_episode(population::Int, topology::SimpleGraph, episode::Int)
+    Random.seed!()
+    initial_cooperators = Game.choose_initial_cooperators(population)
+    model = Game.Model(topology, initial_cooperators)
+    dilemma_range = 0.0:0.1:1.0
+    dg_hist, dr_hist, fc_hist = [], [], []
+
+    for dg in dilemma_range, dr in dilemma_range
+        payoff_matrix = Game.PayoffMatrix(dg, dr)
+        fc = game_loop(model, payoff_matrix, episode)
+        push!(dg_hist, dg)
+        push!(dr_hist, dr)
+        push!(fc_hist, fc)
+    end
+
+    DataFrame(Dg = dg_hist, Dr = dr_hist, Fc = fc_hist) |> CSV.write("./results/episode_$(episode).csv")
 end
 
 end
