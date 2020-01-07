f309192d4b01e321de00792fc41da33e81053674
Keisuke Nagashima <energyrevo21@gmail.com>
2020-01-07 17:22:33 +0900

    fix driver script

--- run.jl
+++ run.jl
@@ -1,16 +1,20 @@
-# For parallel execution, run with $ julia -p (number of process) run.jl
+# For parallel execution,
+# run with $ julia -p (number of process) run.jl
 
 using Distributed
 @everywhere include("simulation.jl")
-using .Simulation
 
-const num_episode = 100
+include("./network_generator.jl")
+using .NetworkGenerator
+
 const population = 10000
 const average_degree = 8
-const topology_type = "Ring"  # ER | ScaleFree | Lattice | Ring
+const topology_type = :ScaleFree  # :ER | :ScaleFree | :Lattice | :Ring
+const topology = NetworkGenerator.generate_topology(population, average_degree, topology_type)
+const num_episode = 1
 
 if isdir("./results") == false
     run(`mkdir results`)
 end
 
-Distributed.pmap(episode -> Simulation.one_episode(episode, population, average_degree, topology_type), 1:num_episode)
+Distributed.pmap(episode -> Simulation.one_episode(population, topology, episode), 1:num_episode)
